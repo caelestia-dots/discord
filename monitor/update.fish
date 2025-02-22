9fd71f9b1c2a19ab43d03d80916bbacd04003004
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2025-02-22 14:21:23 +1100

    use state

--- monitor/update.fish
+++ monitor/update.fish
@@ -7,8 +7,8 @@ function read-scheme -a scheme_path
     end
 end
 
-set -q XDG_CACHE_HOME && set -l cache $XDG_CACHE_HOME || set -l cache $HOME/.cache
-set -l scheme_path $cache/caelestia/scheme/current.txt
+set -q XDG_STATE_HOME && set -l state $XDG_STATE_HOME || set -l state $HOME/.local/state
+set -l scheme_path $state/caelestia/scheme/current.txt
 set -l src (dirname (status filename))/..
 set -l tmp_dir /tmp/caelestia-discord
 
